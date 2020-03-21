5ca1ce2d7b7019731a52aadbbad0c741f49c2b81
Gabriel Sugai <gabrielsugai@gmail.com>
2020-03-21 19:50:00 -0300

    Funcoes do CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,22 @@ class TasksController < ApplicationController
     def create
         @task = Task.new(params.require(:task).permit(:description, :status))
         @task.save #salva no banco a tarefa que criamos
+        redirect_to root_path
     end
+    def edit
+        #utilizando o params para verificar o routes
+        @task = Task.find(params[:id])
+    end
+    def update
+        #toda vez precisamos identificar qual obejto estamos lidando pois toda vez que a action encerra essa informacao e perdida
+        @task = Task.find(params[:id]) 
+        #o rails ja nos entrega um metodo para update
+        @task.update(params.require(:task).permit(:description, :status))
+        redirect_to root_path
+    end
+    def destroy
+        task = Task.find(params[:id]) #nao precisa de @ pois não vai ter view
+        task.destroy #destroi a tarefa encontrada
+        redirect_to root_path
+    end  
 end
