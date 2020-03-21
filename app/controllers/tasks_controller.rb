ca57fe5f8fe3c42a2746eebaa581819099d2f5f6
Gabriel Sugai <gabrielsugai@gmail.com>
2020-03-21 14:56:04 -0300

    Utilizando require para receber os valores

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,7 @@ class TasksController < ApplicationController
     def new
     end
     def create
-        @task = Task.new(description: params[:task][:description],
-        status: params[:task][:status])
+        @task = Task.new(params.require(:task).permit(:description, :status))
         @task.save #salva no banco a tarefa que criamos
     end
 end
