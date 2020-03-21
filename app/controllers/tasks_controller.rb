9ca972998ec4873e18f451d8780f244f47def36b
Gabriel Sugai <gabrielsugai@gmail.com>
2020-03-21 14:48:57 -0300

    Primeira tarefa no Rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
     def new
     end
+    def create
+        @task = Task.new(description: params[:task][:description],
+        status: params[:task][:status])
+        @task.save #salva no banco a tarefa que criamos
+    end
 end
